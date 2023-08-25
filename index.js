eae6e664eb2fe57328b41ab452c4b110ce24859d
Ashish <enigmaticwolf83@gmail.com>
2023-08-25 20:32:59 +0530

    formated inde.js file

--- index.js
+++ index.js
@@ -1,39 +1,42 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const mongoose = require('mongoose');
-const authRoute = require('./routes/authRoute');
-const userRoute = require('./routes/userRoutes');
-const errorHandler = require('./utilis/errorHandlers');
-const courses = require('./routes/courseRoutes');
-// const enrolledCourses = require('./routes/enrollmentRoutes');
-const config = require('./config');
+const cors = require("cors");
+const mongoose = require("mongoose");
+const authRoute = require("./routes/authRoute");
+const userRoute = require("./routes/userRoutes");
+const errorHandler = require("./utilis/errorHandlers");
+const courses = require("./routes/courseRoutes");
+const config = require("./config");
 // const cabRoute = require('./routes/userRoutes');
 app.use(express.json());
-const url = 'mongodb+srv://enigmaticwolf83:449ec5cdMongo@cluster0.5pwrqbs.mongodb.net/?retryWrites=true&w=majority';
 
 
+const url =
+  "mongodb+srv://enigmaticwolf83:449ec5cdMongo@cluster0.5pwrqbs.mongodb.net/?retryWrites=true&w=majority";
 
-app.use(cors({
-    origin: 'https://codinggninjas.onrender.com'
-}));
-app.use('/auth',authRoute);
-app.use('/user',userRoute);
+app.use(
+  cors({
+    origin: "https://codinggninjas.onrender.com",
+  })
+);
+app.use("/auth", authRoute);
+app.use("/user", userRoute);
 app.use(errorHandler);
-app.use('/',courses);
+app.use("/", courses);
 // app.use('/',enrolledCourses)
 // app.use('/',cabRoute);
-  
-app.listen(config.PORT, ()=>{
-    mongoose.connect(url, { 
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }).then(()=>{
-        console.log('App is connected to database');
-    }).catch((error)=>{
-        console.log("Error connecting to database",error);
+
+app.listen(config.PORT, () => {
+  mongoose
+    .connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("App is connected to database");
+    })
+    .catch((error) => {
+      console.log("Error connecting to database", error);
     });
-    console.log(`Server is listening at https://localhost:${config.PORT}`)
+  console.log(`Server is listening at https://localhost:${config.PORT}`);
 });
-
-
